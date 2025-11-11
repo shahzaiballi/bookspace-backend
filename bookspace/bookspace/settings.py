b1c988505e402b9b8fcf8cedf109f91fbcf1f2eb
Sana-Zia <szia5161@gmail.com>
2025-11-11 21:13:20 +0400

    Added Supabase authentication (signup/login) in Accounts app

--- bookspace/bookspace/settings.py
+++ bookspace/bookspace/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # loads .env file
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Accounts'
 ]
 
 MIDDLEWARE = [
